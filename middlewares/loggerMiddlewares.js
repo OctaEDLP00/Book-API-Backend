b1513da6c8b692cd251cc0c1ce2fa900a663b439
DevMetal00 <42822581+OctaEDLP00@users.noreply.github.com>
2025-03-01 04:55:25 -0300

    Chore(loggerMiddlewares): add jsdoc types and add use `import` in entire file

--- middlewares/loggerMiddlewares.js
+++ middlewares/loggerMiddlewares.js
@@ -1,3 +1,8 @@
+/**
+ * @param {import('@types/express').Request} req
+ * @param {import('@types/express').Response} res
+ * @param {import('@types/').NextFunction} next
+ */
 const logger = (req, res, next) => {
   console.log(req.method)
   console.log(req.path)
@@ -6,4 +11,5 @@ const logger = (req, res, next) => {
   next()
 }
 
-module.exports = logger
+export { logger }
+export default logger
